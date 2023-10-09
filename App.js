4c11c46a8bc83c269d6ee4ed76d5d95ea41820b2
Shashi Shekhar <shashiofficialmail@gmail.com>
2023-10-09 12:18:57 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,5 @@
+
+
+const heading = React.createElement("h2", {id: "heading"} , "Hi Shashi you are almost there");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
